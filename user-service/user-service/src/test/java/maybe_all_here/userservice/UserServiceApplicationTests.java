9161c2fe6abfb5ad1d5cdb900c62946aa260ed8a
liveforone <yc4852@gmail.com>
2023-03-20 20:08:30 +0900

    Initial user-service.

---
+++ user-service/user-service/src/test/java/maybe_all_here/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package maybe_all_here.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
