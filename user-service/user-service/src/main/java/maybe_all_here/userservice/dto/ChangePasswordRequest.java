586c245c321a52481095b8191a0943564428a1fa
liveforone <yc4852@gmail.com>
2023-03-20 20:35:31 +0900

    Add member request and response dto.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/dto/ChangePasswordRequest.java
@@ -0,0 +1,17 @@
+package maybe_all_here.userservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "현재 비밀번호를 반드시 입력하세요.")
+    private String oldPassword;
+
+    @NotBlank(message = "새 비밀번호를 반드시 입력하세요.")
+    private String newPassword;
+}
