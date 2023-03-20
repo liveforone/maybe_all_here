a19c7a9e566a8968803da72bc852e684837dc8a4
liveforone <yc4852@gmail.com>
2023-03-20 19:36:34 +0900

    Add AuthorizationHeaderFilter constant.

---
+++ gateway-service/gateway-service/src/main/java/maybe_all_here/gatewayservice/filter/constant/AuthorizationConstant.java
@@ -0,0 +1,8 @@
+package maybe_all_here.gatewayservice.filter.constant;
+
+public class AuthorizationConstant {
+
+    public static final String NO_AUTH_HEADER_MESSAGE = "No Authorization Header!";
+    public static final String BEARER = "Bearer";
+    public static final String NOT_VALID_MESSAGE = "Token is not Valid!";
+}
