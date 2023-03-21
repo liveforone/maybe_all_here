add349835d492ad9d069de69518f4f2bb5badc3e
liveforone <yc4852@gmail.com>
2023-03-21 10:25:36 +0900

    Add circuit breaker.

--- user-service/user-service/src/main/java/maybe_all_here/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/UserServiceApplication.java
@@ -1,13 +1,16 @@
 package maybe_all_here.userservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -17,5 +20,10 @@ public class UserServiceApplication {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
 	}
 }
