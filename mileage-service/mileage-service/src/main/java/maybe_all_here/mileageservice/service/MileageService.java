9dfe4c57238b0eea14ff48d038b768d181539c43
liveforone <yc4852@gmail.com>
2023-03-21 15:48:14 +0900

    Add get mileage by email logic.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/MileageService.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/MileageService.java
@@ -1,7 +1,11 @@
 package maybe_all_here.mileageservice.service;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.mileageservice.domain.Mileage;
+import maybe_all_here.mileageservice.dto.MileageResponse;
 import maybe_all_here.mileageservice.repository.MileageRepository;
+import maybe_all_here.mileageservice.service.util.MileageMapper;
+import maybe_all_here.mileageservice.utility.CommonUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,14 @@ import org.springframework.stereotype.Service;
 public class MileageService {
 
     private final MileageRepository mileageRepository;
+
+    public MileageResponse getMileageByEmail(String email) {
+        Mileage mileage = mileageRepository.getMileageByEmail(email);
+
+        if (CommonUtils.isNull(mileage)) {
+            return null;
+        }
+
+        return MileageMapper.entityToDto(mileage);
+    }
 }
