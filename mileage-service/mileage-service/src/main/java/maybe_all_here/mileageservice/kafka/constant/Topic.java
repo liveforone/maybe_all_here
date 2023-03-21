4b49606f4d971d10e27ed2425b56753484400d6c
liveforone <yc4852@gmail.com>
2023-03-21 12:09:18 +0900

    Add kafka topic constant.

---
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/kafka/constant/Topic.java
@@ -0,0 +1,8 @@
+package maybe_all_here.mileageservice.kafka.constant;
+
+public class Topic {
+
+    public static final String CREATE_MILEAGE = "create-mileage";
+    public static final String INCREASE_MILEAGE = "increase-mileage";
+    public static final String DECREASE_MILEAGE = "decrease-mileage";
+}
