a1af2381a1babd17b3bc5593236aac4f6bf02a91
liveforone <yc4852@gmail.com>
2023-03-21 10:38:48 +0900

    Add kafka user producer.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/kafka/UserProducer.java
@@ -0,0 +1,23 @@
+package maybe_all_here.userservice.kafka;
+
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.userservice.kafka.constant.Topic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    Gson gson = new Gson();
+
+    public void createMileage(String email) {
+        String jsonOrder = gson.toJson(email);
+
+        kafkaTemplate.send(Topic.REQUEST_CREATE_MILEAGE, jsonOrder);
+    }
+}
