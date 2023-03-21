6cb387fa06ed3495cd8f44f1b8c17389749d4827
liveforone <yc4852@gmail.com>
2023-03-21 11:07:07 +0900

    Add member info response dto.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/dto/response/MemberInfoResponse.java
@@ -0,0 +1,20 @@
+package maybe_all_here.userservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import maybe_all_here.userservice.domain.Role;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfoResponse {
+
+    private Long id;
+    private String email;
+    private String realName;
+    private long mileage;
+    private Role auth;
+}
