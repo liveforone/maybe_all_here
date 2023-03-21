bdf2a90297f414a296cf5dc1dd76a3657c518998
liveforone <yc4852@gmail.com>
2023-03-21 12:09:09 +0900

    Add kafka log constant.

---
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/kafka/constant/KafkaLog.java
@@ -0,0 +1,14 @@
+package maybe_all_here.mileageservice.kafka.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum KafkaLog {
+
+    KAFKA_RECEIVE_LOG("Consumer receive Kafka Message -> "),
+    KAFKA_NULL_LOG("!! Kafka Message is Null !!");
+
+    private final String value;
+}
