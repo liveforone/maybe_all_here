065b2eb021a7f6308c7dc76c2efec48ce10b818d
liveforone <yc4852@gmail.com>
2023-03-21 11:06:14 +0900

    Add mileage url constant.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/feignClient/MileageUrl.java
@@ -0,0 +1,7 @@
+package maybe_all_here.userservice.feignClient;
+
+public class MileageUrl {
+
+    public static final String BASE = "mileage-service";
+    public static final String MY_MILEAGE = "/my-mileage/{email}";
+}
