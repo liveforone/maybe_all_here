23fef4d8af9ef322a96b8d00b47a1921581d8dfb
liveforone <yc4852@gmail.com>
2023-03-21 11:06:06 +0900

    Add mileage response dto.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/dto/mileage/MileageResponse.java
@@ -0,0 +1,12 @@
+package maybe_all_here.userservice.dto.mileage;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MileageResponse {
+
+    private String email;
+    private long mileage;
+}
