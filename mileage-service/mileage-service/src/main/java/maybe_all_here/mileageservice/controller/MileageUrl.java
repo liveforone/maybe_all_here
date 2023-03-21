186fc0a4500872512021cbf5477dd63da396f68a
liveforone <yc4852@gmail.com>
2023-03-21 15:48:29 +0900

    Add mileage url constant.

---
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/controller/MileageUrl.java
@@ -0,0 +1,7 @@
+package maybe_all_here.mileageservice.controller;
+
+public class MileageUrl {
+
+    public static final String MY_MILEAGE = "/my-mileage/{email}";
+    public static final String MILEAGE_INFO = "/mileage-info/{email}";
+}
