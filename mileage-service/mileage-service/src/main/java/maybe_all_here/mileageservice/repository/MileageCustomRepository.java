877a652e671ce6e8083c5bd213d1eda3bc7b4193
liveforone <yc4852@gmail.com>
2023-03-21 15:47:41 +0900

    Add get mileage by email query.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/repository/MileageCustomRepository.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/repository/MileageCustomRepository.java
@@ -1,6 +1,8 @@
 package maybe_all_here.mileageservice.repository;
 
+import maybe_all_here.mileageservice.domain.Mileage;
+
 public interface MileageCustomRepository {
 
-
+    Mileage getMileageByEmail(String email);
 }
--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/repository/MileageRepositoryImpl.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/repository/MileageRepositoryImpl.java
@@ -1,11 +1,21 @@
 package maybe_all_here.mileageservice.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.mileageservice.domain.Mileage;
+import maybe_all_here.mileageservice.domain.QMileage;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class MileageRepositoryImpl implements MileageCustomRepository {
 
+    private final JPAQueryFactory queryFactory;
+    QMileage mileage = QMileage.mileage;
 
+    public Mileage getMileageByEmail(String email) {
+        return queryFactory.selectFrom(mileage)
+                .where(mileage.email.eq(email))
+                .fetchOne();
+    }
 }
