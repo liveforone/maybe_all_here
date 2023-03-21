40a44e5819858ff7f8e2f53b8114a1120ecfcc75
liveforone <yc4852@gmail.com>
2023-03-21 15:47:55 +0900

    Add entity to dto mapper.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/util/MileageMapper.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/util/MileageMapper.java
@@ -1,12 +1,20 @@
 package maybe_all_here.mileageservice.service.util;
 
 import maybe_all_here.mileageservice.domain.Mileage;
+import maybe_all_here.mileageservice.dto.MileageResponse;
 
 public class MileageMapper {
 
     public static Mileage createMileage(String email) {
         return Mileage.builder()
                 .email(email)
+                .build();
+    }
+
+    public static MileageResponse entityToDto(Mileage mileage) {
+        return MileageResponse.builder()
+                .email(mileage.getEmail())
+                .mileagePoint(mileage.getMileagePoint())
                 .build();
     }
 }
