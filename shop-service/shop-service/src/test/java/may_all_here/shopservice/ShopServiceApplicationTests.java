1245835136a8b5dce3cd9d495ac43ad3a01c162d
liveforone <yc4852@gmail.com>
2023-03-22 20:47:15 +0900

    Initial shop-service.

---
+++ shop-service/shop-service/src/test/java/may_all_here/shopservice/ShopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package may_all_here.shopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
