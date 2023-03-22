660fa15da177437f46ea136d5a29bc04a971941e
liveforone <yc4852@gmail.com>
2023-03-22 22:19:26 +0900

    Add member url and parameter constant.

---
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/MemberUrl.java
@@ -0,0 +1,7 @@
+package may_all_here.shopservice.feignClient;
+
+public class MemberUrl {
+
+    public static final String BASE = "member-service";
+    public static final String USER_INFO = "/user-info/{email}";
+}
---
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/constant/MemberParam.java
@@ -0,0 +1,6 @@
+package may_all_here.shopservice.feignClient.constant;
+
+public class MemberParam {
+
+    public static final String EMAIL = "email";
+}
