b02efa6e52a4ae978178e4252f41bc7c814a634c
liveforone <yc4852@gmail.com>
2023-03-23 10:56:58 +0900

    Add find shop by email method.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/repository/ShopCustomRepository.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/repository/ShopCustomRepository.java
@@ -1,6 +1,8 @@
 package may_all_here.shopservice.repository;
 
+import may_all_here.shopservice.domain.Shop;
+
 public interface ShopCustomRepository {
 
-
+    Shop findShopByEmail(String email);
 }
--- shop-service/shop-service/src/main/java/may_all_here/shopservice/repository/ShopRepositoryImpl.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/repository/ShopRepositoryImpl.java
@@ -2,11 +2,20 @@ package may_all_here.shopservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import may_all_here.shopservice.domain.QShop;
+import may_all_here.shopservice.domain.Shop;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class ShopRepositoryImpl {
+public class ShopRepositoryImpl implements ShopCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QShop shop = QShop.shop;
+
+    public Shop findShopByEmail(String email) {
+        return queryFactory.selectFrom(shop)
+                .where(shop.email.eq(email))
+                .fetchOne();
+    }
 }
