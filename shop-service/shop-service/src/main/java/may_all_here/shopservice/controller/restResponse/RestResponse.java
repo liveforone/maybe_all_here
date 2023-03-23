d96519d6e1acc205453869b90a7ed4e290de36aa
liveforone <yc4852@gmail.com>
2023-03-23 11:01:14 +0900

    Add valid error, duplicate email and create shop success rest response.

---
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/restResponse/RestResponse.java
@@ -0,0 +1,37 @@
+package may_all_here.shopservice.controller.restResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
+
+public class RestResponse {
+
+    public static ResponseEntity<?> validError(BindingResult bindingResult) {
+        String errorMessage = Objects
+                .requireNonNull(bindingResult.getFieldError())
+                .getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessage);
+    }
+
+    public static ResponseEntity<?> notSellerError() {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ResponseMessage.NOT_SELLER.getValue());
+    }
+
+    public static ResponseEntity<?> duplicateUser() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ResponseMessage.DUPLICATE_USER.getValue());
+    }
+
+    public static ResponseEntity<?> createShopSuccess() {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ResponseMessage.SHOP_CREATE_SUCCESS.getValue());
+    }
+}
