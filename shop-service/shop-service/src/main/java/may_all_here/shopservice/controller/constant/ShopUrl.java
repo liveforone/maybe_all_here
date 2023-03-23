983ef832e09ca47cf72eb2fd9329e5019ce7e6e2
liveforone <yc4852@gmail.com>
2023-03-23 11:31:14 +0900

    Add shop detail and info url.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ShopUrl.java
@@ -3,4 +3,6 @@ package may_all_here.shopservice.controller.constant;
 public class ShopUrl {
 
     public static final String CREATE_SHOP = "/create-shop";
+    public static final String SHOP_DETAIL = "/shop/{shopId}";
+    public static final String SHOP_SELLER_PAGE = "/shop/info/{email}";
 }
