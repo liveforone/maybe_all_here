cd78d820cb1f97dd47f2cc845d9f3cf34b5e0a1b
liveforone <yc4852@gmail.com>
2023-03-23 12:33:50 +0900

    Rename feign service.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/MemberFeignService.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/MemberFeignService.java
@@ -2,13 +2,15 @@ package may_all_here.shopservice.feignClient;
 
 import may_all_here.shopservice.dto.member.MemberResponse;
 import may_all_here.shopservice.feignClient.constant.MemberParam;
+import may_all_here.shopservice.feignClient.constant.MemberUrl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = MemberUrl.BASE)
 public interface MemberFeignService {
 
-    @GetMapping(MemberUrl.USER_INFO)
+    @PostMapping(MemberUrl.USER_INFO)
     MemberResponse getMemberInfo(@PathVariable(MemberParam.EMAIL) String email);
 }
--- shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/constant/MemberUrl.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/constant/MemberUrl.java
@@ -2,6 +2,6 @@ package may_all_here.shopservice.feignClient.constant;
 
 public class MemberUrl {
 
-    public static final String BASE = "member-service";
+    public static final String BASE = "user-service";
     public static final String USER_INFO = "/user-info/{email}";
 }
