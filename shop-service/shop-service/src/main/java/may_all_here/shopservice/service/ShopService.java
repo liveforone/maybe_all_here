980eaaa8f0d180c0fe91848db9469f8e66b53973
liveforone <yc4852@gmail.com>
2023-03-23 11:00:38 +0900

    Add create shop logic.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/service/ShopService.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/service/ShopService.java
@@ -1,7 +1,9 @@
 package may_all_here.shopservice.service;
 
 import lombok.RequiredArgsConstructor;
+import may_all_here.shopservice.dto.shop.ShopRequest;
 import may_all_here.shopservice.repository.ShopRepository;
+import may_all_here.shopservice.service.util.ShopMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ShopService {
 
     private final ShopRepository shopRepository;
+
+    @Transactional
+    public Long createShop(ShopRequest shopRequest) {
+        return shopRepository
+                .save(ShopMapper.dtoToEntity(shopRequest))
+                .getId();
+    }
 }
