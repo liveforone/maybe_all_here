8b503c3dc39fba1b010a9c384064bb875f50140d
liveforone <yc4852@gmail.com>
2023-03-23 11:30:40 +0900

    Add shop response mapper.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/service/util/ShopMapper.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/service/util/ShopMapper.java
@@ -2,6 +2,7 @@ package may_all_here.shopservice.service.util;
 
 import may_all_here.shopservice.domain.Shop;
 import may_all_here.shopservice.dto.shop.ShopRequest;
+import may_all_here.shopservice.dto.shop.ShopResponse;
 
 public class ShopMapper {
 
@@ -12,6 +13,14 @@ public class ShopMapper {
                 .address(shopRequest.getAddress())
                 .tel(shopRequest.getTel())
                 .email(shopRequest.getEmail())
+                .build();
+    }
+
+    public static ShopResponse entityToDtoDetail(Shop shop) {
+        return ShopResponse.builder()
+                .shopName(shop.getShopName())
+                .address(shop.getAddress())
+                .tel(shop.getTel())
                 .build();
     }
 }
