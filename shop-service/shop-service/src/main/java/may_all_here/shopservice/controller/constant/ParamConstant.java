2cf943fd8d19797ce48e295f2ea3636792b60ab6
liveforone <yc4852@gmail.com>
2023-03-23 11:35:24 +0900

    Add email param.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ParamConstant.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ParamConstant.java
@@ -3,4 +3,5 @@ package may_all_here.shopservice.controller.constant;
 public class ParamConstant {
 
     public static final String SHOP_ID = "shopId";
+    public static final String EMAIL = "email";
 }
