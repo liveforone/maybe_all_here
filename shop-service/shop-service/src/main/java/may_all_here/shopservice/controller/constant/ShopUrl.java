7805a487bdcddb869f71329cbcce8ebfb29ec925
liveforone <yc4852@gmail.com>
2023-03-26 21:18:22 +0900

    Add change name, address and tel url constant.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ShopUrl.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ShopUrl.java
@@ -5,4 +5,7 @@ public class ShopUrl {
     public static final String CREATE_SHOP = "/create-shop";
     public static final String SHOP_DETAIL = "/shop/{shopId}";
     public static final String SHOP_SELLER_PAGE = "/shop/info/{email}";
+    public static final String CHANGE_SHOP_NAME = "/shop/change-name/{shopId}";
+    public static final String CHANGE_SHOP_ADDRESS = "/shop/change-address/{shopId}";
+    public static final String CHANGE_SHOP_TEL = "/shop/change-tel/{shopId}";
 }
