01502f282e8178e9e9fd3379eb0948876d672383
liveforone <yc4852@gmail.com>
2023-03-26 21:43:40 +0900

    Add shop name update success log.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/restResponse/ResponseMessage.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/restResponse/ResponseMessage.java
@@ -10,7 +10,8 @@ public enum ResponseMessage {
     NOT_SELLER("상점 개설은 셀러 회원만 가능합니다.\n일반 회원은 상점 개설이 불가능합니다."),
     DUPLICATE_USER("상점은 한 번만 개설 가능합니다."),
     SHOP_CREATE_SUCCESS("상점 개설에 성공하였습니다.\n축하합니다."),
-    SHOP_IS_NULL("상점이 존재하지 않습니다.");
+    SHOP_IS_NULL("상점이 존재하지 않습니다."),
+    SHOP_NAME_UPDATE_SUCCESS("상호명 변경에 성공하였습니다.");
 
     private final String value;
 }
