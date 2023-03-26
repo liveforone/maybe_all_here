7e736fd068576066e75fb0807636eae6fbf62b43
liveforone <yc4852@gmail.com>
2023-03-26 21:58:00 +0900

    Add update tel log.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ControllerLog.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ControllerLog.java
@@ -9,7 +9,8 @@ public enum ControllerLog {
 
     SHOP_CREATE_SUCCESS("상점 개설 성공 || Shop id : "),
     UPDATE_SHOP_NAME_SUCCESS("상호명 변경 성공 || Shop id : "),
-    UPDATE_SHOP_ADDRESS_SUCCESS("상점 주소 변경 성공 || Shop id : ");
+    UPDATE_SHOP_ADDRESS_SUCCESS("상점 주소 변경 성공 || Shop id : "),
+    UPDATE_SHOP_TEL_SUCCESS("상점 전화번호 변경 성공 || Shop id : ");
 
     private final String value;
 }
