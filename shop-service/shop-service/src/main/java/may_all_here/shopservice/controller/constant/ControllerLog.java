d4635766a371e1849c0543ac891fd048e1dfa00b
liveforone <yc4852@gmail.com>
2023-03-26 21:43:30 +0900

    Add shop name update log.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ControllerLog.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/controller/constant/ControllerLog.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ControllerLog {
 
-    SHOP_CREATE_SUCCESS("상점 개설 성공, Shop id : ");
+    SHOP_CREATE_SUCCESS("상점 개설 성공 || Shop id : "),
+    UPDATE_SHOP_NAME_SUCCESS("상호명 변경 성공 || Shop id : ");
 
     private final String value;
 }
