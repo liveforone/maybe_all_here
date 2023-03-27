856f6f17addbc58623f9a4973417911a50ad66bc
liveforone <yc4852@gmail.com>
2023-03-27 23:16:57 +0900

    Add seller info response dto.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/dto/shop/SellerInfoResponse.java
@@ -0,0 +1,15 @@
+package maybe_all_here.itemservice.dto.shop;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SellerInfoResponse {
+
+    private String email;
+}
