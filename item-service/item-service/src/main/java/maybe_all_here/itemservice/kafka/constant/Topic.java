cec64582d89dd6df98f12c07a508ae3cdcc4b4dd
liveforone <yc4852@gmail.com>
2023-03-27 23:16:32 +0900

    Add kafka topic constant.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
@@ -0,0 +1,7 @@
+package maybe_all_here.itemservice.kafka.constant;
+
+public class Topic {
+
+    public static final String SAVE_FILE = "save-file";
+    public static final String DELETE_FILE = "delete-file";
+}
