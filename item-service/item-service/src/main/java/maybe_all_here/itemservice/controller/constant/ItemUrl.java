b68cec29d587f4b35f2445c83c0ecbdb6b66e69a
liveforone <yc4852@gmail.com>
2023-03-28 14:20:39 +0900

    Add item detail url.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
@@ -3,5 +3,6 @@ package maybe_all_here.itemservice.controller.constant;
 public class ItemUrl {
 
     public static final String ITEM_HOME = "/item-home";
+    public static final String ITEM_DETAIL = "/item/{itemId}";
     public static final String CREATE_ITEM = "/create-item/{shopId}";
 }
