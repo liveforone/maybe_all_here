7e7fb9f74c45b0bab185b9e89afb5e64192e2b3f
liveforone <yc4852@gmail.com>
2023-03-28 15:48:04 +0900

    Add delete item controller log.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ControllerLog.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ControllerLog.java
@@ -11,7 +11,8 @@ public enum ControllerLog {
     EDIT_TITLE_SUCCESS("Edit Item title success || Item id : "),
     EDIT_CONTENT_SUCCESS("Edit Item content success || Item id : "),
     EDIT_PRICE_SUCCESS("Edit Item price success || Item id : "),
-    EDIT_REMAINING_SUCCESS("Edit Item remaining success || Item id : ");
+    EDIT_REMAINING_SUCCESS("Edit Item remaining success || Item id : "),
+    DELETE_ITEM_SUCCESS("Delete Item success || Item id : ");
 
     private final String value;
 }
