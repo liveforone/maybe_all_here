515f37416fb03bd10eb66273e456b69633d8c9a2
liveforone <yc4852@gmail.com>
2023-03-28 14:00:00 +0900

    Add paging param constant.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ParamConstant.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ParamConstant.java
@@ -3,4 +3,6 @@ package maybe_all_here.itemservice.controller.constant;
 public class ParamConstant {
 
     public static final String SHOP_ID = "shopId";
+    public static final String LAST_ID = "lastId";
+    public static final String PAGE_SIZE = "pageSize";
 }
