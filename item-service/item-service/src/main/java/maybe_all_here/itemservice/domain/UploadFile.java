ee36678bdcb485d363b8ab8dc615f3ec9a684de6
liveforone <yc4852@gmail.com>
2023-03-28 17:06:14 +0900

    Add upload file entity.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/domain/UploadFile.java
@@ -0,0 +1,30 @@
+package maybe_all_here.itemservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class UploadFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String saveFileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Item item;
+
+    @Builder
+    public UploadFile(Long id, String saveFileName, Item item) {
+        this.id = id;
+        this.saveFileName = saveFileName;
+        this.item = item;
+    }
+}
