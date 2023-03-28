0e8c0c1637b3c16c3354370e74f0d71e6b0f789f
liveforone <yc4852@gmail.com>
2023-03-28 17:33:00 +0900

    Add upload file repos.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/uploadFile/UploadFileCustomRepository.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/uploadFile/UploadFileCustomRepository.java
@@ -1,4 +1,13 @@
 package maybe_all_here.itemservice.repository.uploadFile;
 
+import maybe_all_here.itemservice.domain.Item;
+import maybe_all_here.itemservice.domain.UploadFile;
+
+import java.util.List;
+
 public interface UploadFileCustomRepository {
+
+    List<UploadFile> findFilesByItem(Item item);
+
+    void deleteBulkFileByItem(Long itemId);
 }
--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/uploadFile/UploadFileRepositoryImpl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/uploadFile/UploadFileRepositoryImpl.java
@@ -2,11 +2,33 @@ package maybe_all_here.itemservice.repository.uploadFile;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.itemservice.domain.Item;
+import maybe_all_here.itemservice.domain.QItem;
+import maybe_all_here.itemservice.domain.QUploadFile;
+import maybe_all_here.itemservice.domain.UploadFile;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class UploadFileRepositoryImpl {
+public class UploadFileRepositoryImpl implements UploadFileCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QUploadFile uploadFile = QUploadFile.uploadFile;
+    QItem qItem = QItem.item;
+
+    public List<UploadFile> findFilesByItem(Item item) {
+        return queryFactory.selectFrom(uploadFile)
+                .join(uploadFile.item, qItem).fetchJoin()
+                .where(qItem.eq(item))
+                .orderBy(uploadFile.id.asc())
+                .fetch();
+    }
+
+    public void deleteBulkFileByItem(Long itemId) {
+        queryFactory.delete(uploadFile)
+                .where(uploadFile.item.id.eq(itemId))
+                .execute();
+    }
 }
