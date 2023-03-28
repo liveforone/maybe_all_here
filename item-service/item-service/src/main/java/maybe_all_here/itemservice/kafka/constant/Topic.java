578c90d8a39edf3d0d989793d353a5080d638c4e
liveforone <yc4852@gmail.com>
2023-03-28 12:54:01 +0900

    Add increase good and bad topic constant.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
@@ -6,4 +6,6 @@ public class Topic {
     public static final String DELETE_FILE = "delete-file";
 
     public static final String DECREASE_REMAINING = "decrease-remaining";
+    public static final String ITEM_IS_GOOD = "item-is-good";
+    public static final String ITEM_IS_BAD = "item-is-bad";
 }
