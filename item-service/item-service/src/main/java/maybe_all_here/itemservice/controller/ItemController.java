dab54426406cae91597081815b39784daeab6d25
liveforone <yc4852@gmail.com>
2023-03-28 14:24:17 +0900

    Add item detail controller api.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
@@ -11,6 +11,7 @@ import maybe_all_here.itemservice.dto.item.ItemRequest;
 import maybe_all_here.itemservice.dto.item.ItemResponse;
 import maybe_all_here.itemservice.kafka.ItemProducer;
 import maybe_all_here.itemservice.service.ItemService;
+import maybe_all_here.itemservice.validator.ItemValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -25,10 +26,12 @@ public class ItemController {
 
     private final ItemService itemService;
     private final ItemProducer itemProducer;
+    private final ItemValidator itemValidator;
 
     /*
     상품 페이지
-    셀러 상품 페이지
+    일반 상점 상품리스트
+    셀러 상점 상품리스트
     검색(페이징)
     수정(제목, 내용, 가격, 재고)
     삭제(파일 -> 리뷰 -> 상품 순으로)
@@ -42,6 +45,18 @@ public class ItemController {
         List<ItemResponse> itemHome = itemService.getItemHome(lastId, pageSize);
 
         return ResponseEntity.ok(itemHome);
+    }
+
+    @GetMapping(ItemUrl.ITEM_DETAIL)
+    public ResponseEntity<?> itemDetail(
+            @PathVariable(ParamConstant.ITEM_ID) Long itemId
+    ) {
+        if (itemValidator.isNullItem(itemId)) {
+            return RestResponse.itemIsNull();
+        }
+
+        ItemResponse item = itemService.getItemById(itemId);
+        return ResponseEntity.ok(item);
     }
 
     @PostMapping(ItemUrl.CREATE_ITEM)
