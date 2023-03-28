1edd9153ddbae264a1e2147797d5dd862e915708
liveforone <yc4852@gmail.com>
2023-03-28 13:46:35 +0900

    Add create item method in service.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/service/ItemService.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/service/ItemService.java
@@ -1,7 +1,9 @@
 package maybe_all_here.itemservice.service;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.itemservice.dto.item.ItemRequest;
 import maybe_all_here.itemservice.repository.ItemRepository;
+import maybe_all_here.itemservice.service.util.ItemMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long createItem(ItemRequest itemRequest, Long shopId) {
+        itemRequest.setShopId(shopId);
+
+        return itemRepository
+                .save(ItemMapper.dtoToEntity(itemRequest))
+                .getId();
+    }
 }
