7f2e830b75dd1664bef895ae769da17dfae9e3e0
liveforone <yc4852@gmail.com>
2023-03-28 14:08:52 +0900

    Add get item home method.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/service/ItemService.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/service/ItemService.java
@@ -2,10 +2,13 @@ package maybe_all_here.itemservice.service;
 
 import lombok.RequiredArgsConstructor;
 import maybe_all_here.itemservice.dto.item.ItemRequest;
+import maybe_all_here.itemservice.dto.item.ItemResponse;
 import maybe_all_here.itemservice.repository.ItemRepository;
 import maybe_all_here.itemservice.service.util.ItemMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    public List<ItemResponse> getItemHome(Long lastId, int pageSize) {
+        return ItemMapper.entityToDtoList(itemRepository.findItemHome(lastId, pageSize));
+    }
 
     @Transactional
     public Long createItem(ItemRequest itemRequest, Long shopId) {
