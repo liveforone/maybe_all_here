ea457caf9b37a74ca284c9dc2c558c3e1bda7004
liveforone <yc4852@gmail.com>
2023-03-28 13:59:35 +0900

    Add item home url.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
@@ -2,5 +2,6 @@ package maybe_all_here.itemservice.controller.constant;
 
 public class ItemUrl {
 
+    public static final String ITEM_HOME = "/item-home";
     public static final String CREATE_ITEM = "/create-item/{shopId}";
 }
