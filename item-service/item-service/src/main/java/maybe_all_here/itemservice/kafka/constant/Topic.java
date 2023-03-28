57f8dd1df1c16ec51808ee49068bb5a9e62fc17d
liveforone <yc4852@gmail.com>
2023-03-28 17:02:07 +0900

    Remove file topics.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
@@ -2,9 +2,6 @@ package maybe_all_here.itemservice.kafka.constant;
 
 public class Topic {
 
-    public static final String SAVE_FILE = "save-file";
-    public static final String DELETE_FILE = "delete-file";
-
     public static final String DECREASE_REMAINING = "decrease-remaining";
     public static final String ITEM_IS_GOOD = "item-is-good";
     public static final String ITEM_IS_BAD = "item-is-bad";
