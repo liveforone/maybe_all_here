253351890e2ed02992874d3c6ee6f38b06668a25
liveforone <yc4852@gmail.com>
2023-03-28 15:36:30 +0900

    Add edit remaining log.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ControllerLog.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ControllerLog.java
@@ -10,7 +10,8 @@ public enum ControllerLog {
     CREATE_ITEM_SUCCESS("Create Item Success || Item id : "),
     EDIT_TITLE_SUCCESS("Edit Item title success || Item id : "),
     EDIT_CONTENT_SUCCESS("Edit Item content success || Item id : "),
-    EDIT_PRICE_SUCCESS("Edit Item price success || Item id : ");
+    EDIT_PRICE_SUCCESS("Edit Item price success || Item id : "),
+    EDIT_REMAINING_SUCCESS("Edit Item remaining success || Item id : ");
 
     private final String value;
 }
