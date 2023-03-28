50fd3c0934b53480d5dc222f62558b485d93d21d
liveforone <yc4852@gmail.com>
2023-03-28 14:45:36 +0900

    Add seller items list url.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/constant/ItemUrl.java
@@ -5,4 +5,6 @@ public class ItemUrl {
     public static final String ITEM_HOME = "/item-home";
     public static final String ITEM_DETAIL = "/item/{itemId}";
     public static final String CREATE_ITEM = "/create-item/{shopId}";
+    public static final String SHOP_ITEMS_LIST = "/items/{shopId}";
+    public static final String SELLER_ITEMS_LIST = "/items/seller/{shopId}";
 }
