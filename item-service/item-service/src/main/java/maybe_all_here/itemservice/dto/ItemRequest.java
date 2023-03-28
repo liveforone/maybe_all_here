e07037b17645d00e3a5f31bf53541371584146ab
liveforone <yc4852@gmail.com>
2023-03-28 13:10:27 +0900

    Add validation message in request dto.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/dto/ItemRequest.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/dto/ItemRequest.java
@@ -1,5 +1,6 @@
 package maybe_all_here.itemservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +10,16 @@ public class ItemRequest {
 
     private Long id;
     private Long shopId;
+
+    @NotBlank(message = "상품 이름은 반드시 작성해주세요.")
     private String title;
+
+    @NotBlank(message = "상품 상세 내용은 반드시 작성해주세요.")
     private String content;
+
+    @NotBlank(message = "상품 가격은 반드시 입력해주세요.")
     private long price;
+
+    @NotBlank(message = "상품 재고 수량은 반드시 입력해주세요.")
     private long remaining;
 }
