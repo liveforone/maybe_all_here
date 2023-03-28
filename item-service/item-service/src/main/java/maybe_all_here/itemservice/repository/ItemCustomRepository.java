56cc4c2b42f6899e4cadfc1fce75d2ce615021f9
liveforone <yc4852@gmail.com>
2023-03-28 12:59:30 +0900

    Add increase good and bad query method.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemCustomRepository.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemCustomRepository.java
@@ -5,4 +5,8 @@ import maybe_all_here.itemservice.dto.ItemRemainingRequest;
 public interface ItemCustomRepository {
 
     void decreaseRemaining(ItemRemainingRequest itemRemainingRequest);
+
+    void increaseGood(Long itemId);
+
+    void increaseBad(Long itemId);
 }
--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemRepositoryImpl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemRepositoryImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 public class ItemRepositoryImpl implements ItemCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    private static final long RECOMMEND = 1;
     QItem item = QItem.item;
 
     public void decreaseRemaining(ItemRemainingRequest itemRemainingRequest) {
@@ -20,6 +21,20 @@ public class ItemRepositoryImpl implements ItemCustomRepository {
                         item.remaining.add(-itemRemainingRequest.getOrderQuantity())
                 )
                 .where(item.id.eq(itemRemainingRequest.getItemId()))
+                .execute();
+    }
+
+    public void increaseGood(Long itemId) {
+        queryFactory.update(item)
+                .set(item.good, item.good.add(RECOMMEND))
+                .where(item.id.eq(itemId))
+                .execute();
+    }
+
+    public void increaseBad(Long itemId) {
+        queryFactory.update(item)
+                .set(item.bad, item.bad.add(RECOMMEND))
+                .where(item.id.eq(itemId))
                 .execute();
     }
 }
