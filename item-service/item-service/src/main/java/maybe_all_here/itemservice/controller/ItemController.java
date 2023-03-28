6d240200e0164f5bd6a9ad0ee17a3bca670fef4e
liveforone <yc4852@gmail.com>
2023-03-28 14:10:41 +0900

    Add item home api.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
@@ -8,14 +8,12 @@ import maybe_all_here.itemservice.controller.constant.ItemUrl;
 import maybe_all_here.itemservice.controller.constant.ParamConstant;
 import maybe_all_here.itemservice.controller.restResponse.RestResponse;
 import maybe_all_here.itemservice.dto.item.ItemRequest;
+import maybe_all_here.itemservice.dto.item.ItemResponse;
 import maybe_all_here.itemservice.kafka.ItemProducer;
 import maybe_all_here.itemservice.service.ItemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -29,13 +27,22 @@ public class ItemController {
     private final ItemProducer itemProducer;
 
     /*
-    상품 홈(페이징)
     상품 페이지
     셀러 상품 페이지
     검색(페이징)
     수정(제목, 내용, 가격, 재고)
-    삭제
+    삭제(파일 -> 리뷰 -> 상품 순으로)
      */
+
+    @GetMapping(ItemUrl.ITEM_HOME)
+    public ResponseEntity<?> itemHome(
+            @RequestParam(name = ParamConstant.LAST_ID) Long lastId,
+            @RequestParam(name = ParamConstant.PAGE_SIZE) int pageSize
+    ) {
+        List<ItemResponse> itemHome = itemService.getItemHome(lastId, pageSize);
+
+        return ResponseEntity.ok(itemHome);
+    }
 
     @PostMapping(ItemUrl.CREATE_ITEM)
     public ResponseEntity<?> createItem(
