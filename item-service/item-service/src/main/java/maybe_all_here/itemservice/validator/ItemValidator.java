318cba5fd614c10a3b5d92da99852ea9dcadb7ca
liveforone <yc4852@gmail.com>
2023-03-28 14:20:04 +0900

    Add item validator.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/validator/ItemValidator.java
@@ -0,0 +1,20 @@
+package maybe_all_here.itemservice.validator;
+
+import lombok.RequiredArgsConstructor;
+import maybe_all_here.itemservice.domain.Item;
+import maybe_all_here.itemservice.repository.ItemRepository;
+import maybe_all_here.itemservice.utility.CommonUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ItemValidator {
+
+    private final ItemRepository itemRepository;
+
+    public boolean isNullItem(Long itemId) {
+        Item item = itemRepository.findOneById(itemId);
+
+        return CommonUtils.isNull(item);
+    }
+}
