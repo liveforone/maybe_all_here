48c6bc3af3136fdf707b603ff43646138ae6fb6f
liveforone <yc4852@gmail.com>
2023-03-28 11:57:58 +0900

    Add decrease item remaining query method.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemCustomRepository.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemCustomRepository.java
@@ -1,4 +1,8 @@
 package maybe_all_here.itemservice.repository;
 
+import maybe_all_here.itemservice.dto.ItemRemainingRequest;
+
 public interface ItemCustomRepository {
+
+    void decreaseRemaining(ItemRemainingRequest itemRemainingRequest);
 }
--- item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemRepositoryImpl.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/repository/ItemRepositoryImpl.java
@@ -2,11 +2,24 @@ package maybe_all_here.itemservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.itemservice.domain.QItem;
+import maybe_all_here.itemservice.dto.ItemRemainingRequest;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class ItemRepositoryImpl {
+public class ItemRepositoryImpl implements ItemCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QItem item = QItem.item;
+
+    public void decreaseRemaining(ItemRemainingRequest itemRemainingRequest) {
+        queryFactory.update(item)
+                .set(
+                        item.remaining,
+                        item.remaining.add(-itemRemainingRequest.getOrderQuantity())
+                )
+                .where(item.id.eq(itemRemainingRequest.getItemId()))
+                .execute();
+    }
 }
