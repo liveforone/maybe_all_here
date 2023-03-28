8173bf70fe3576bf350b81b2297b948409fe899b
liveforone <yc4852@gmail.com>
2023-03-28 13:53:52 +0900

    Add create item controller.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/ItemController.java
@@ -1,9 +1,24 @@
 package maybe_all_here.itemservice.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.itemservice.controller.constant.ControllerLog;
+import maybe_all_here.itemservice.controller.constant.ItemUrl;
+import maybe_all_here.itemservice.controller.constant.ParamConstant;
+import maybe_all_here.itemservice.controller.restResponse.RestResponse;
+import maybe_all_here.itemservice.dto.item.ItemRequest;
+import maybe_all_here.itemservice.kafka.ItemProducer;
 import maybe_all_here.itemservice.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +26,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemProducer itemProducer;
+
+    /*
+    상품 홈(페이징)
+    상품 페이지
+    셀러 상품 페이지
+    검색(페이징)
+    수정(제목, 내용, 가격, 재고)
+    삭제
+     */
+
+    @PostMapping(ItemUrl.CREATE_ITEM)
+    public ResponseEntity<?> createItem(
+            @RequestPart @Valid ItemRequest itemRequest,
+            BindingResult bindingResult,
+            @RequestPart List<MultipartFile> uploadFile,
+            @PathVariable(ParamConstant.SHOP_ID) Long shopId
+    ) {
+        if (bindingResult.hasErrors()) {
+            return RestResponse.validError(bindingResult);
+        }
+
+        Long itemId = itemService.createItem(itemRequest, shopId);
+        itemProducer.saveFile(uploadFile, itemId);
+        log.info(ControllerLog.CREATE_ITEM_SUCCESS.getValue() + itemId);
+
+        return RestResponse.createItemSuccess();
+    }
 }
