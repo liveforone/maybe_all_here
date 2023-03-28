5bf6d9ca7c0b3b6c4dbb96e93a3f35b18c69df0c
liveforone <yc4852@gmail.com>
2023-03-28 17:11:45 +0900

    Add upload file request dto.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/dto/uploadFile/UploadFileRequest.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/dto/uploadFile/UploadFileRequest.java
@@ -1,11 +1,15 @@
 package maybe_all_here.itemservice.dto.uploadFile;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import maybe_all_here.itemservice.domain.Item;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class UploadFileRequest {
 
     private Long id;
