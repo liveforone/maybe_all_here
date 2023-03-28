8bd904f598cbe5176d923f5518113a2bddaed69e
liveforone <yc4852@gmail.com>
2023-03-28 11:30:28 +0900

    Add fk updatable constraints.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/domain/Mileage.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/domain/Mileage.java
@@ -15,7 +15,7 @@ public class Mileage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, updatable = false)
     private String email;
 
     private long mileagePoint;
