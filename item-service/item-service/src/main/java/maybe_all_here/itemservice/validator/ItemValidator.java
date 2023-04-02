8fa71638c25eca55335d460f1febffa04bc2af00
liveforone <yc4852@gmail.com>
2023-04-02 21:24:27 +0900

    Add member auth check validator.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/validator/ItemValidator.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/validator/ItemValidator.java
@@ -1,10 +1,13 @@
 package maybe_all_here.itemservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.itemservice.authentication.Role;
 import maybe_all_here.itemservice.domain.Item;
 import maybe_all_here.itemservice.repository.item.ItemRepository;
 import maybe_all_here.itemservice.utility.CommonUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -16,5 +19,9 @@ public class ItemValidator {
         Item item = itemRepository.findOneById(itemId);
 
         return CommonUtils.isNull(item);
+    }
+
+    public boolean isNotSeller(String auth) {
+        return !Objects.equals(auth, Role.SELLER.getValue());
     }
 }
