65c5c49f10b20b2115820b281856f7ab99a4f1f8
liveforone <yc4852@gmail.com>
2023-04-02 20:38:58 +0900

    Remove member feign.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/constant/MemberParam.java
+++
@@ -1,6 +0,0 @@
-package may_all_here.shopservice.feignClient.constant;
-
-public class MemberParam {
-
-    public static final String EMAIL = "email";
-}
--- shop-service/shop-service/src/main/java/may_all_here/shopservice/feignClient/constant/MemberUrl.java
+++
@@ -1,7 +0,0 @@
-package may_all_here.shopservice.feignClient.constant;
-
-public class MemberUrl {
-
-    public static final String BASE = "user-service";
-    public static final String USER_INFO = "/user-info/{email}";
-}
