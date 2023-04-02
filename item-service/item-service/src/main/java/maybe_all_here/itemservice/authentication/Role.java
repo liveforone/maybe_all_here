d60fff1cd1f6cb500bcc6c1cc669e013551ad6cc
liveforone <yc4852@gmail.com>
2023-04-02 21:17:08 +0900

    Add role enum class.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/authentication/Role.java
@@ -0,0 +1,15 @@
+package maybe_all_here.itemservice.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+
+    MEMBER("ROLE_MEMBER"),
+    SELLER("ROLE_SELLER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String value;
+}
