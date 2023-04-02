0655d4a92341adadc49b57bbd612e653dbe5dde4
liveforone <yc4852@gmail.com>
2023-04-02 21:24:07 +0900

    Add not seller rest message.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/restResponse/RestMessage.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/controller/restResponse/RestMessage.java
@@ -14,7 +14,8 @@ public enum RestMessage {
     EDIT_CONTENT_SUCCESS("수정한 내용이 정상적으로 반영되었습니다."),
     EDIT_PRICE_SUCCESS("수정한 가격이 정상적으로 반영되었습니다."),
     EDIT_REMAINING_SUCCESS("수정한 수량이 정상적으로 반영되었습니다."),
-    DELETE_ITEM_SUCCESS("상품이 정상적으로 삭제되었습니다.");
+    DELETE_ITEM_SUCCESS("상품이 정상적으로 삭제되었습니다."),
+    NOT_SELLER("셀러 회원이 아닙니다.");
 
     private final String value;
 }
