4823cd77048cbcccff5728abdfc4593c1f28be6d
liveforone <yc4852@gmail.com>
2023-04-02 20:41:15 +0900

    Refactor check member auth and email logic. Cause I added authentication info.

--- shop-service/shop-service/src/main/java/may_all_here/shopservice/validator/ShopValidator.java
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/validator/ShopValidator.java
@@ -1,38 +1,27 @@
 package may_all_here.shopservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import may_all_here.shopservice.authentication.Role;
 import may_all_here.shopservice.domain.Shop;
-import may_all_here.shopservice.dto.member.MemberResponse;
-import may_all_here.shopservice.dto.member.Role;
 import may_all_here.shopservice.dto.shop.ShopRequest;
-import may_all_here.shopservice.feignClient.MemberFeignService;
 import may_all_here.shopservice.repository.ShopRepository;
 import may_all_here.shopservice.utility.CommonUtils;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class ShopValidator {
 
     private final ShopRepository shopRepository;
-    private final MemberFeignService memberFeignService;
-    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
-    public boolean isNotSeller(ShopRequest shopRequest) {
-        String CIRCUIT_LOG = "shop-circuit-breaker";
-
-        MemberResponse memberInfo = circuitBreakerFactory
-                .create(CIRCUIT_LOG)
-                .run(() -> memberFeignService.getMemberInfo(shopRequest.getEmail()),
-                        throwable -> new MemberResponse()
-                );
-
-        return memberInfo.getAuth() != Role.SELLER;
+    public boolean isNotSeller(String auth) {
+        return !Objects.equals(auth, Role.SELLER.getValue());
     }
 
-    public boolean isDuplicateUser(ShopRequest shopRequest) {
-        Shop shop = shopRepository.findShopByEmail(shopRequest.getEmail());
+    public boolean isDuplicateUser(String email) {
+        Shop shop = shopRepository.findShopByEmail(email);
 
         return !CommonUtils.isNull(shop);
     }
