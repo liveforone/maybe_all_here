01eae58d552b807a64c4ccfa847da16b69f8a750
liveforone <yc4852@gmail.com>
2023-04-03 17:20:10 +0900

    Add create review method in controller.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/ReviewService.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/ReviewService.java
@@ -1,7 +1,10 @@
 package maybe_all_here.reviewservice.service;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.reviewservice.dto.review.ReviewRequest;
+import maybe_all_here.reviewservice.kafka.ReviewProducer;
 import maybe_all_here.reviewservice.repository.ReviewRepository;
+import maybe_all_here.reviewservice.service.util.ReviewMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final ReviewProducer reviewProducer;
 
+    @Transactional
+    public void createReview(ReviewRequest reviewRequest, String email, Long itemId) {
+        reviewRequest.setEmail(email);
+        reviewRequest.setItemId(itemId);
 
+//        reviewProducer.sendRecommendState(reviewRequest);
+        reviewRepository.save(ReviewMapper.dtoToEntity(reviewRequest));
+    }
 }
