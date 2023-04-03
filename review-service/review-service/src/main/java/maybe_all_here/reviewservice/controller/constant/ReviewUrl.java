cbe84eaac28987f143c3e66088cbc5497ce2ae1d
liveforone <yc4852@gmail.com>
2023-04-03 18:33:32 +0900

    Add edit review url.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
@@ -5,4 +5,5 @@ public class ReviewUrl {
     public static final String REVIEWS_BELONG_TO_ITEM = "/reviews/{itemId}";
     public static final String REVIEW_DETAIL = "/review/{reviewId}";
     public static final String CREATE_REVIEW = "/create-review/{itemId}";
+    public static final String EDIT_REVIEW = "/edit-review/{reviewId}";
 }
