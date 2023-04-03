7cb1d7154f16c01d54098dfa9d5f044c4e7b0152
liveforone <yc4852@gmail.com>
2023-04-03 17:37:59 +0900

    Add get reviews by itemId url.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
@@ -2,5 +2,6 @@ package maybe_all_here.reviewservice.controller.constant;
 
 public class ReviewUrl {
 
+    public static final String REVIEWS_BELONG_TO_ITEM = "/reviews/{itemId}";
     public static final String CREATE_REVIEW = "/create-review/{itemId}";
 }
