f7fcbbaadf23a9609a16c3ab7c1b3b7c61614548
liveforone <yc4852@gmail.com>
2023-04-03 14:28:20 +0900

    Add blank message in request dto.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/dto/review/ReviewRequest.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/dto/review/ReviewRequest.java
@@ -15,5 +15,6 @@ public class ReviewRequest {
     @NotBlank(message = "리뷰는 공백으로 제출할 수 없습니다.\n회원님의 생각을 잘 담아주세요.")
     private String content;
 
+    @NotBlank(message = "추천/비추천은 반드시 표시해야합니다.")
     private String recommend;
 }
