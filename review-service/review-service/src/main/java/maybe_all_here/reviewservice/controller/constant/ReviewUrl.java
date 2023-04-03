39149a14bdae356ea31e2fbb9f3232bcc266dd1c
liveforone <yc4852@gmail.com>
2023-04-03 18:12:34 +0900

    Add review detail url.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
@@ -3,5 +3,6 @@ package maybe_all_here.reviewservice.controller.constant;
 public class ReviewUrl {
 
     public static final String REVIEWS_BELONG_TO_ITEM = "/reviews/{itemId}";
+    public static final String REVIEW_DETAIL = "/review/{reviewId}";
     public static final String CREATE_REVIEW = "/create-review/{itemId}";
 }
