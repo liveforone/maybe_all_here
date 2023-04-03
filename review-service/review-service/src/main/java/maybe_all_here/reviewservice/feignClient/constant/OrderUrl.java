7f456219c0e9ba96621ada4abd5f204fdf29191d
liveforone <yc4852@gmail.com>
2023-04-03 14:46:10 +0900

    Add order url.

---
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/feignClient/constant/OrderUrl.java
@@ -0,0 +1,7 @@
+package maybe_all_here.reviewservice.feignClient.constant;
+
+public class OrderUrl {
+
+    public static final String BASE = "order-service";
+    public static final String ORDER_INFO = "/order-info/{email}/and/{itemId}";
+}
