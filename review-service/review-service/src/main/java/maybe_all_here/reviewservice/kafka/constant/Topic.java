09a5f46b3deb78ba6745d1f1b2b7e224c37bee97
liveforone <yc4852@gmail.com>
2023-04-03 10:33:32 +0900

    Add kafka topic.

---
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/constant/Topic.java
@@ -0,0 +1,9 @@
+package maybe_all_here.reviewservice.kafka.constant;
+
+public class Topic {
+
+    public static final String REMOVE_BELONG_REVIEW = "remove-belong-review";
+
+    public static final String ITEM_IS_GOOD = "item-is-good";
+    public static final String ITEM_IS_BAD = "item-is-bad";
+}
