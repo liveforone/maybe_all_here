ffa2580d28efb52090c3d6273122201494d238e9
liveforone <yc4852@gmail.com>
2023-04-03 16:14:22 +0900

    Add send recommend state method.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewProducer.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewProducer.java
@@ -3,9 +3,13 @@ package maybe_all_here.reviewservice.kafka;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.reviewservice.dto.review.Recommend;
+import maybe_all_here.reviewservice.dto.review.ReviewRequest;
 import maybe_all_here.reviewservice.kafka.constant.Topic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -14,14 +18,25 @@ public class ReviewProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
+
+    public void sendRecommendState(ReviewRequest reviewRequest) {
+        String recommend = reviewRequest.getRecommend();
+        Long itemId = reviewRequest.getItemId();
+
+        if (Objects.equals(recommend, Recommend.RECOMMEND.getValue())) {
+            recommendItem(itemId);
+        } else {
+            notRecommendItem(itemId);
+        }
+    }
     
-    public void recommendItem(Long itemId) {
+    private void recommendItem(Long itemId) {
         String jsonOrder = gson.toJson(itemId);
 
         kafkaTemplate.send(Topic.ITEM_IS_GOOD, jsonOrder);
     }
 
-    public void notRecommendItem(Long itemId) {
+    private void notRecommendItem(Long itemId) {
         String jsonOrder = gson.toJson(itemId);
 
         kafkaTemplate.send(Topic.ITEM_IS_BAD, jsonOrder);
