53c33b57244586d0389a679c6077081c207b83fe
liveforone <yc4852@gmail.com>
2023-04-03 10:26:31 +0900

    Add remove belong review topic.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/constant/Topic.java
@@ -5,4 +5,6 @@ public class Topic {
     public static final String DECREASE_REMAINING = "decrease-remaining";
     public static final String ITEM_IS_GOOD = "item-is-good";
     public static final String ITEM_IS_BAD = "item-is-bad";
+
+    public static final String REMOVE_BELONG_REVIEW = "remove-belong-review";
 }
