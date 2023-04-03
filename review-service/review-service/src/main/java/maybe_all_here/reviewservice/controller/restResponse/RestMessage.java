45a0bbcbf13c5a0a0cd06511ced0b3a6b5d58be0
liveforone <yc4852@gmail.com>
2023-04-03 19:04:26 +0900

    Refactor not owner rest message.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/restResponse/RestMessage.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/restResponse/RestMessage.java
@@ -11,7 +11,7 @@ public enum RestMessage {
     CANCEL_ORDER("주문이 취소되어 리뷰작성이 불가능합니다."),
     CREATE_REVIEW_SUCCESS("리뷰가 성공적으로 등록되었습니다."),
     REVIEW_IS_NULL("리뷰가 존재하지 않습니다."),
-    NOT_OWNER("수정은 작성자만 가능합니다."),
+    NOT_OWNER("작성자가 아닙니다."),
     EDIT_REVIEW_SUCCESS("리뷰 수정에 성공했습니다.");
 
     private final String value;
