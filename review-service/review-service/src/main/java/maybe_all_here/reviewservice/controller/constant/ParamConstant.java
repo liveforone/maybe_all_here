ec063b7b0be7662319c18bc626ca7c361f74a458
liveforone <yc4852@gmail.com>
2023-04-03 17:38:17 +0900

    Add last id and page size constant.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ParamConstant.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ParamConstant.java
@@ -3,4 +3,6 @@ package maybe_all_here.reviewservice.controller.constant;
 public class ParamConstant {
 
     public static final String ITEM_ID = "itemId";
+    public static final String LAST_ID = "lastId";
+    public static final String PAGE_SIZE = "pageSize";
 }
