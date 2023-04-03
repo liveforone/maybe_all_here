e798e57fa047b72aab76e6154df6e8fcf10612f5
liveforone <yc4852@gmail.com>
2023-04-03 10:28:09 +0900

    Add remove belong review producer method.

--- item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/ItemProducer.java
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/kafka/ItemProducer.java
@@ -3,6 +3,7 @@ package maybe_all_here.itemservice.kafka;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.itemservice.kafka.constant.Topic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,9 @@ public class ItemProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
 
-//    public void createMileage(String email) {
-//        String jsonOrder = gson.toJson(email);
-//
-//        kafkaTemplate.send(Topic.CREATE_MILEAGE, jsonOrder);
-//    }
+    public void removeBelongReview(Long itemId) {
+        String jsonOrder = gson.toJson(itemId);
+
+        kafkaTemplate.send(Topic.REMOVE_BELONG_REVIEW, jsonOrder);
+    }
 }
