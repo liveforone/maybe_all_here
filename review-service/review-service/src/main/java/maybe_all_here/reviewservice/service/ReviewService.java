bbd8a4d9b16c9ce8e86beff9256b2e072726aec3
liveforone <yc4852@gmail.com>
2023-04-03 17:34:13 +0900

    Add get reviews by itemId method in service.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/ReviewService.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/ReviewService.java
@@ -2,11 +2,14 @@ package maybe_all_here.reviewservice.service;
 
 import lombok.RequiredArgsConstructor;
 import maybe_all_here.reviewservice.dto.review.ReviewRequest;
+import maybe_all_here.reviewservice.dto.review.ReviewResponse;
 import maybe_all_here.reviewservice.kafka.ReviewProducer;
 import maybe_all_here.reviewservice.repository.ReviewRepository;
 import maybe_all_here.reviewservice.service.util.ReviewMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,12 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final ReviewProducer reviewProducer;
+
+    public List<ReviewResponse> getReviewsByItemId(Long itemId, Long lastId, int pageSize) {
+        return ReviewMapper.entityToDtoList(
+                reviewRepository.findReviewsByItemId(itemId, lastId, pageSize)
+        );
+    }
 
     @Transactional
     public void createReview(ReviewRequest reviewRequest, String email, Long itemId) {
