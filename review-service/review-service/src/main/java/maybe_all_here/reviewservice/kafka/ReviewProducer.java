193002f493e4daf7d267ecaee2c34785bc82c74b
liveforone <yc4852@gmail.com>
2023-04-03 13:20:21 +0900

    Add item recommend and not recommend kafka producer method.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewProducer.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewProducer.java
@@ -3,6 +3,7 @@ package maybe_all_here.reviewservice.kafka;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.reviewservice.kafka.constant.Topic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,16 @@ public class ReviewProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
+    
+    public void recommendItem(Long itemId) {
+        String jsonOrder = gson.toJson(itemId);
 
-//    public void createMileage(String email) {
-//        String jsonOrder = gson.toJson(email);
-//
-//        kafkaTemplate.send(Topic.CREATE_MILEAGE, jsonOrder);
-//    }
+        kafkaTemplate.send(Topic.ITEM_IS_GOOD, jsonOrder);
+    }
+
+    public void notRecommendItem(Long itemId) {
+        String jsonOrder = gson.toJson(itemId);
+
+        kafkaTemplate.send(Topic.ITEM_IS_BAD, jsonOrder);
+    }
 }
