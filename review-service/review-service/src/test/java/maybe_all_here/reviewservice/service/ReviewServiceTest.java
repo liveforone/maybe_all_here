e4d9719be4fce95e0211014fd51f6708ad6cb42f
liveforone <yc4852@gmail.com>
2023-04-03 19:46:40 +0900

    Add edit review test.

--- review-service/review-service/src/test/java/maybe_all_here/reviewservice/service/ReviewServiceTest.java
+++ review-service/review-service/src/test/java/maybe_all_here/reviewservice/service/ReviewServiceTest.java
@@ -1,6 +1,7 @@
 package maybe_all_here.reviewservice.service;
 
 import jakarta.persistence.EntityManager;
+import maybe_all_here.reviewservice.dto.review.ReviewEditRequest;
 import maybe_all_here.reviewservice.dto.review.ReviewRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,13 @@ class ReviewServiceTest {
 
     @Autowired
     EntityManager em;
+
+    private Long createReview(Long itemId, String email, String content, String recommend) {
+        ReviewRequest reviewRequest = new ReviewRequest();
+        reviewRequest.setContent(content);
+        reviewRequest.setRecommend(recommend);
+        return reviewService.createReview(reviewRequest, email, itemId);
+    }
 
     @Test
     @Transactional
@@ -43,7 +51,27 @@ class ReviewServiceTest {
     }
 
     @Test
-    void editReviewById() {
+    @Transactional
+    void editReviewByIdTest() {
+        //given
+        Long itemId = 1L;
+        String email = "aa1234@gmail.com";
+        String content = "test_content";
+        String recommend = "true";
+        Long reviewId = createReview(itemId, email, content, recommend);
+
+        //when
+        String updatedContent = "updated content";
+        ReviewEditRequest request = new ReviewEditRequest();
+        request.setContent(updatedContent);
+        reviewService.editReviewById(request, reviewId);
+        em.flush();
+        em.clear();
+
+        //then
+        Assertions
+                .assertThat(reviewService.getReviewById(reviewId).getContent())
+                .isEqualTo(updatedContent);
     }
 
     @Test
