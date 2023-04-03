79c0037ddb84a989989880416b92af2653e39b3f
liveforone <yc4852@gmail.com>
2023-04-03 17:33:48 +0900

    Add null check logic in mapper.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/util/ReviewMapper.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/service/util/ReviewMapper.java
@@ -3,7 +3,9 @@ package maybe_all_here.reviewservice.service.util;
 import maybe_all_here.reviewservice.domain.Review;
 import maybe_all_here.reviewservice.dto.review.ReviewRequest;
 import maybe_all_here.reviewservice.dto.review.ReviewResponse;
+import maybe_all_here.reviewservice.utility.CommonUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +22,10 @@ public class ReviewMapper {
     }
 
     public static ReviewResponse entityToDtoDetail(Review review) {
+        if (CommonUtils.isNull(review)) {
+            return new ReviewResponse();
+        }
+
         return ReviewResponse.builder()
                 .id(review.getId())
                 .email(review.getEmail())
@@ -29,6 +35,10 @@ public class ReviewMapper {
     }
 
     public static List<ReviewResponse> entityToDtoList(List<Review> reviews) {
+        if (reviews.isEmpty()) {
+            return new ArrayList<>();
+        }
+
         return reviews
                 .stream()
                 .map(ReviewMapper::entityToDtoDetail)
