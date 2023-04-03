b2c9370c10c4afdb087cc885ddb30009b3239c98
liveforone <yc4852@gmail.com>
2023-04-03 13:38:40 +0900

    Add review request.

---
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/dto/ReviewRequest.java
@@ -0,0 +1,19 @@
+package maybe_all_here.reviewservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ReviewRequest {
+
+    private Long id;
+    private Long itemId;
+    private String email;
+
+    @NotBlank(message = "리뷰는 공백으로 제출할 수 없습니다.\n회원님의 생각을 잘 담아주세요.")
+    private String content;
+
+    private String recommend;
+}
