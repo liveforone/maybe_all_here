a9ee218c03bae983250dc9875d9ca764a71d32a9
liveforone <yc4852@gmail.com>
2023-04-03 18:16:23 +0900

    Add review null check validator.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/validator/ReviewValidator.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/validator/ReviewValidator.java
@@ -1,14 +1,24 @@
 package maybe_all_here.reviewservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.reviewservice.domain.Review;
 import maybe_all_here.reviewservice.dto.order.OrderProvideResponse;
 import maybe_all_here.reviewservice.dto.order.OrderState;
+import maybe_all_here.reviewservice.repository.ReviewRepository;
 import maybe_all_here.reviewservice.utility.CommonUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ReviewValidator {
+
+    private final ReviewRepository reviewRepository;
+
+    public boolean isNullReview(Long reviewId) {
+        Review review = reviewRepository.findOneById(reviewId);
+
+        return CommonUtils.isNull(review);
+    }
 
     public boolean isCancelOrder(OrderProvideResponse order) {
         if (order.getOrderState() == OrderState.CANCEL) {
