204c9e4a73f5b130e378c5e98079a45aff2c987d
liveforone <yc4852@gmail.com>
2023-04-03 16:17:08 +0900

    Add order info feign service.

---
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/feignClient/OrderFeignService.java
@@ -0,0 +1,18 @@
+package maybe_all_here.reviewservice.feignClient;
+
+import maybe_all_here.reviewservice.dto.order.OrderProvideResponse;
+import maybe_all_here.reviewservice.feignClient.constant.OrderUrl;
+import maybe_all_here.reviewservice.feignClient.constant.ProvideParamConstant;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = OrderUrl.BASE)
+public interface OrderFeignService {
+
+    @PostMapping(OrderUrl.ORDER_INFO)
+    OrderProvideResponse getOrderInfo(
+            @PathVariable(ProvideParamConstant.EMAIL) String email,
+            @PathVariable(ProvideParamConstant.ITEM_ID) Long itemId
+    );
+}
