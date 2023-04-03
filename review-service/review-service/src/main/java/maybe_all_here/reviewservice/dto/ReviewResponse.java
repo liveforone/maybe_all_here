13ae98764558cd2d9ad4ded0f2c07a75910e499e
liveforone <yc4852@gmail.com>
2023-04-03 13:38:46 +0900

    Add review response.

---
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/dto/ReviewResponse.java
@@ -0,0 +1,18 @@
+package maybe_all_here.reviewservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewResponse {
+
+    private Long id;
+    private String email;
+    private String content;
+    private String recommend;
+}
