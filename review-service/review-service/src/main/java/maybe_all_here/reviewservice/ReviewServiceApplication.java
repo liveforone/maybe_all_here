268dfcb205a44327dbf72ee06302a95beaa027b8
liveforone <yc4852@gmail.com>
2023-04-03 13:43:44 +0900

    Add open feign of stating bean in main class.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/ReviewServiceApplication.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/ReviewServiceApplication.java
@@ -1,14 +1,17 @@
 package maybe_all_here.reviewservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableJpaAuditing
 public class ReviewServiceApplication {
 
@@ -19,5 +22,10 @@ public class ReviewServiceApplication {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLeve() {
+		return Logger.Level.FULL;
 	}
 }
