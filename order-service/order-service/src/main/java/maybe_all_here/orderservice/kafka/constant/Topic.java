694d84e77c24bdea85603aa2e6333984006db161
liveforone <yc4852@gmail.com>
2023-04-04 14:02:47 +0900

    Add kafka topic.

---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/kafka/constant/Topic.java
@@ -0,0 +1,9 @@
+package maybe_all_here.orderservice.kafka.constant;
+
+public class Topic {
+
+    public static final String DECREASE_REMAINING = "decrease-remaining";
+    public static final String INCREASE_MILEAGE = "increase-mileage";
+    public static final String DECREASE_MILEAGE = "decrease-mileage";
+    public static final String REMOVE_REVIEW_BELONG_ORDER = "remove-review-belong-order";
+}
