0a4cdc6ef65d4dc424654767832f863e4a7e8146
liveforone <yc4852@gmail.com>
2023-04-04 20:09:36 +0900

    Add get order by email and item id service method.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/service/OrderService.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/service/OrderService.java
@@ -1,8 +1,10 @@
 package maybe_all_here.orderservice.service;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.orderservice.dto.order.OrderProvideResponse;
 import maybe_all_here.orderservice.kafka.OrderProducer;
 import maybe_all_here.orderservice.repository.OrderRepository;
+import maybe_all_here.orderservice.service.util.OrderMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,4 +15,10 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderProducer orderProducer;
+
+    public OrderProvideResponse getOrderByEmailAndItemId(String email, Long itemId) {
+        return OrderMapper.entityToProvideDto(
+                orderRepository.findOneByEmailAndItemId(email, itemId)
+        );
+    }
 }
