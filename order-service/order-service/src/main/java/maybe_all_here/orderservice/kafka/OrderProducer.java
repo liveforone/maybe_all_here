6adffe0a619e459c5f6ef8f55da7563263fb6fe6
liveforone <yc4852@gmail.com>
2023-04-04 18:57:07 +0900

    Add decrease item remaining kafka method.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/kafka/OrderProducer.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/kafka/OrderProducer.java
@@ -3,6 +3,8 @@ package maybe_all_here.orderservice.kafka;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.orderservice.dto.item.ItemRemainingRequest;
+import maybe_all_here.orderservice.dto.order.OrderRequest;
 import maybe_all_here.orderservice.dto.review.RemoveReviewBelongOrderRequest;
 import maybe_all_here.orderservice.kafka.constant.KafkaLog;
 import maybe_all_here.orderservice.kafka.constant.Topic;
@@ -17,15 +19,25 @@ public class OrderProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
 
+    public void decreaseRemaining(OrderRequest orderRequest) {
+        ItemRemainingRequest request = ItemRemainingRequest.builder()
+                .itemId(orderRequest.getItemId())
+                .orderQuantity(orderRequest.getOrderQuantity())
+                .build();
 
+        String jsonOrder = gson.toJson(request);
+        String topic = Topic.DECREASE_REMAINING;
+        kafkaTemplate.send(topic, jsonOrder);
+        log.info(KafkaLog.KAFKA_SEND_LOG.getValue() + topic);
+    }
     
     public void removeReviewBelongOrder(String email, Long itemId) {
         RemoveReviewBelongOrderRequest request = RemoveReviewBelongOrderRequest.builder()
                 .email(email)
                 .itemId(itemId)
                 .build();
-        String jsonOrder = gson.toJson(request);
 
+        String jsonOrder = gson.toJson(request);
         String topic = Topic.REMOVE_REVIEW_BELONG_ORDER;
         kafkaTemplate.send(topic, jsonOrder);
         log.info(KafkaLog.KAFKA_SEND_LOG.getValue() + topic);
