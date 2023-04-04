4169bd959fa078c9ea0550bd38d6ae1ac6a7b560
liveforone <yc4852@gmail.com>
2023-04-04 20:05:38 +0900

    Add builder in order provide dto.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderProvideResponse.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderProvideResponse.java
@@ -1,11 +1,15 @@
 package maybe_all_here.orderservice.dto.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import maybe_all_here.orderservice.domain.OrderState;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderProvideResponse {
 
     private Long id;
