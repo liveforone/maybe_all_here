9d2db2f1be5384ed1c1f5bff52aa5338b595c987
liveforone <yc4852@gmail.com>
2023-04-04 10:48:29 +0900

    Add remove review belong to order topic.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/constant/Topic.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/constant/Topic.java
@@ -2,7 +2,7 @@ package maybe_all_here.reviewservice.kafka.constant;
 
 public class Topic {
 
-
+    public static final String REMOVE_REVIEW_BELONG_ORDER = "remove-review-belong-order";
     public static final String REMOVE_REVIEW_BELONG_ITEM = "remove-review-belong-item";
 
     public static final String ITEM_IS_GOOD = "item-is-good";
