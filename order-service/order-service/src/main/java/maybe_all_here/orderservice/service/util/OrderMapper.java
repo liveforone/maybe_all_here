b4c86865653eb2a94e10faafa9bf2e0d07dac537
liveforone <yc4852@gmail.com>
2023-04-04 20:09:14 +0900

    Add null check in mapper.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/service/util/OrderMapper.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/service/util/OrderMapper.java
@@ -2,10 +2,15 @@ package maybe_all_here.orderservice.service.util;
 
 import maybe_all_here.orderservice.domain.Orders;
 import maybe_all_here.orderservice.dto.order.OrderProvideResponse;
+import maybe_all_here.orderservice.utility.CommonUtils;
 
 public class OrderMapper {
 
     public static OrderProvideResponse entityToProvideDto(Orders orders) {
+        if (CommonUtils.isNull(orders)) {
+            return new OrderProvideResponse();
+        }
+
         return OrderProvideResponse.builder()
                 .id(orders.getId())
                 .itemId(orders.getItemId())
