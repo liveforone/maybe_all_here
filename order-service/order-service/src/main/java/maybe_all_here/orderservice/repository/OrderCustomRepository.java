542107a80c7edb5c616bcd08ac8ac7cbb40a7d1d
liveforone <yc4852@gmail.com>
2023-04-04 20:03:27 +0900

    Add find order by email and item id for provide.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/repository/OrderCustomRepository.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/repository/OrderCustomRepository.java
@@ -1,4 +1,8 @@
 package maybe_all_here.orderservice.repository;
 
+import maybe_all_here.orderservice.domain.Orders;
+
 public interface OrderCustomRepository {
+
+    Orders findOneByEmailAndItemId(String email, Long itemId);
 }
--- order-service/order-service/src/main/java/maybe_all_here/orderservice/repository/OrderRepositoryImpl.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/repository/OrderRepositoryImpl.java
@@ -2,6 +2,7 @@ package maybe_all_here.orderservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.orderservice.domain.Orders;
 import maybe_all_here.orderservice.domain.QOrders;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,12 @@ public class OrderRepositoryImpl implements OrderCustomRepository {
     private final JPAQueryFactory queryFactory;
     QOrders orders = QOrders.orders;
 
-
+    public Orders findOneByEmailAndItemId(String email, Long itemId) {
+        return queryFactory.selectFrom(orders)
+                .where(
+                        orders.email.eq(email),
+                        orders.itemId.eq(itemId)
+                )
+                .fetchOne();
+    }
 }
