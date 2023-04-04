3483abafce67a24c75aca8691f573e59d2016ff7
liveforone <yc4852@gmail.com>
2023-04-04 19:34:46 +0900

    Add mileage info for get method.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/MileageFeignService.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/MileageFeignService.java
@@ -4,12 +4,16 @@ import maybe_all_here.orderservice.dto.mileage.MileageResponse;
 import maybe_all_here.orderservice.feignClient.constant.MileageParamConstant;
 import maybe_all_here.orderservice.feignClient.constant.MileageUrl;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = MileageUrl.BASE)
 public interface MileageFeignService {
 
+    @GetMapping(MileageUrl.MILEAGE_INFO)
+    MileageResponse getMileageInfoGet(@PathVariable(MileageParamConstant.EMAIL) String email);
+
     @PostMapping(MileageUrl.MILEAGE_INFO)
-    MileageResponse getMileageInfo(@PathVariable(MileageParamConstant.EMAIL) String email);
+    MileageResponse getMileageInfoPost(@PathVariable(MileageParamConstant.EMAIL) String email);
 }
