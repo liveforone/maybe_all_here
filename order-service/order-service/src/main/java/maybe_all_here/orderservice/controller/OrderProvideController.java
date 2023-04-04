fcda2dab92719e11ae65c75c46851570518907ea
liveforone <yc4852@gmail.com>
2023-04-04 19:45:25 +0900

    Add order provide controller.

---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/OrderProvideController.java
@@ -0,0 +1,9 @@
+package maybe_all_here.orderservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderProvideController {
+}
