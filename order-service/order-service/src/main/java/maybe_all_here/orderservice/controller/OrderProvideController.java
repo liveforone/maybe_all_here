293b889688b69f180f346219c1ee0de1032ea540
liveforone <yc4852@gmail.com>
2023-04-04 20:10:18 +0900

    Add order info method in provide controller.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/OrderProvideController.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/OrderProvideController.java
@@ -1,9 +1,27 @@
 package maybe_all_here.orderservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.orderservice.controller.constant.OrderProvideUrl;
+import maybe_all_here.orderservice.controller.constant.ProvideParamConstant;
+import maybe_all_here.orderservice.dto.order.OrderProvideResponse;
+import maybe_all_here.orderservice.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderProvideController {
+
+    private final OrderService orderService;
+
+    @GetMapping(OrderProvideUrl.ORDER_INFO)
+    public ResponseEntity<?> getOrderInfo(
+            @PathVariable(ProvideParamConstant.EMAIL) String email,
+            @PathVariable(ProvideParamConstant.ITEM_ID) Long itemId
+    ) {
+        OrderProvideResponse order = orderService.getOrderByEmailAndItemId(email, itemId);
+        return ResponseEntity.ok(order);
+    }
 }
