77192d7ea83f6fb34c075b73d0d23b2755681271
liveforone <yc4852@gmail.com>
2023-04-04 14:04:44 +0900

    Add order provide param and url constant.

---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/OrderProvideUrl.java
@@ -0,0 +1,6 @@
+package maybe_all_here.orderservice.controller.constant;
+
+public class OrderProvideUrl {
+
+    public static final String ORDER_INFO = "/order-info?email={email}&item={itemId}";
+}
---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/ProvideParamConstant.java
@@ -0,0 +1,7 @@
+package maybe_all_here.orderservice.controller.constant;
+
+public class ProvideParamConstant {
+
+    public static final String EMAIL = "email";
+    public static final String ITEM_ID = "itemId";
+}
