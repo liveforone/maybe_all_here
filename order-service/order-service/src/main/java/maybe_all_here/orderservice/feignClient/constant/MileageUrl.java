93dad4c16d899234cde3837dcf2e1d6f0148195a
liveforone <yc4852@gmail.com>
2023-04-04 14:07:43 +0900

    Add mileage base url.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/constant/MileageUrl.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/constant/MileageUrl.java
@@ -2,5 +2,6 @@ package maybe_all_here.orderservice.feignClient.constant;
 
 public class MileageUrl {
 
+    public static final String BASE = "mileage-service";
     public static final String MILEAGE_INFO = "/mileage-info/{email}";
 }
