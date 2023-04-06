673afa6f5eb940b283cc7faced240cacc791e459
liveforone <yc4852@gmail.com>
2023-04-06 16:49:13 +0900

    Add validation in request dto.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderRequest.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderRequest.java
@@ -1,5 +1,8 @@
 package maybe_all_here.orderservice.dto.order;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +17,18 @@ public class OrderRequest {
 
     private Long id;
     private String itemTitle;
+
+    @Positive(message = "주문 수량은 반드시 0 이상이여야 합니다.")
+    @NotNull(message = "주문하려는 수량을 반드시 기재해주세요.")
     private long orderQuantity;
+
     private long totalPrice;
     private long discountedPrice;
+
+    @PositiveOrZero(message = "사용하려는 마일리지는 양수로 작성해주세요.")
+    @NotNull(message = "사용하려는 마일리지는 반드시 기재해주세요.")
     private long spentMileage;
+
     private String email;
     private Long itemId;
     private OrderState orderState;
