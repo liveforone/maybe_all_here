216d13f9cbf602929ba9f9b92075059a53da7576
liveforone <yc4852@gmail.com>
2023-04-06 11:51:43 +0900

    Add circuit log.

---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/CircuitLog.java
@@ -0,0 +1,13 @@
+package maybe_all_here.orderservice.controller.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CircuitLog {
+
+    ORDER_CIRCUIT_LOG("order-circuit-log");
+
+    private final String value;
+}
