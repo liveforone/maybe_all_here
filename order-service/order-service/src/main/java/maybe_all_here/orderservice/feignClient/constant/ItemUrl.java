d38d6f102b8d2b4d823a779e56075a4ac72018a8
liveforone <yc4852@gmail.com>
2023-04-06 11:36:19 +0900

    Add base url.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/constant/ItemUrl.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/feignClient/constant/ItemUrl.java
@@ -2,5 +2,7 @@ package maybe_all_here.orderservice.feignClient.constant;
 
 public class ItemUrl {
 
+    public static final String BASE = "item-service";
+
     public static final String ITEM_PROVIDE_DETAIL = "/item-provide/{itemId}";
 }
