879dea0f035afb723a6893af1900ba569b70a5e2
liveforone <yc4852@gmail.com>
2023-04-06 16:27:59 +0900

    Add order test.

---
+++ order-service/order-service/src/test/java/maybe_all_here/orderservice/service/OrderServiceTest.java
@@ -0,0 +1,56 @@
+package maybe_all_here.orderservice.service;
+
+import jakarta.persistence.EntityManager;
+import maybe_all_here.orderservice.dto.item.ItemProvideResponse;
+import maybe_all_here.orderservice.dto.order.OrderRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @Transactional
+    void orderTest() {
+        //given
+        long orderQuantity = 2;
+        long itemPrice = 20000;
+        long mileage = 5000;
+        String email = "aaaa1234@gmail.com";
+        OrderRequest orderRequest = OrderRequest.builder()
+                .orderQuantity(orderQuantity)
+                .spentMileage(mileage)
+                .build();
+        ItemProvideResponse item = ItemProvideResponse.builder()
+                .id(1L)
+                .title("test_item_title")
+                .itemPrice(itemPrice)
+                .remaining(20)
+                .build();
+
+        //when
+        Long orderId = orderService.order(orderRequest, item, email);
+        em.flush();
+        em.clear();
+
+        //then
+        long totalPrice = 40000;
+        long finalPrice = 35000;
+        Assertions
+                .assertThat(orderService.getOrderById(orderId).getTotalPrice())
+                .isEqualTo(totalPrice);
+
+        Assertions
+                .assertThat(orderService.getOrderById(orderId).getDiscountedPrice())
+                .isEqualTo(finalPrice);
+    }
+}
