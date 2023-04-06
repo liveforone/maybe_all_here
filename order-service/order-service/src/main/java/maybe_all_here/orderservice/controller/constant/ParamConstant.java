aa373595b78fb9f40ca74e15fe480e4ab5a62264
liveforone <yc4852@gmail.com>
2023-04-06 16:35:05 +0900

    Add order id param constant.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/ParamConstant.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/ParamConstant.java
@@ -2,5 +2,6 @@ package maybe_all_here.orderservice.controller.constant;
 
 public class ParamConstant {
 
+    public static final String ORDER_ID = "orderId";
     public static final String ITEM_ID = "itemId";
 }
