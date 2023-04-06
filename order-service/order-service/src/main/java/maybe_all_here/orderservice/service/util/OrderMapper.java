75002d01bce1edf46a4a286a8429f003d88872eb
liveforone <yc4852@gmail.com>
2023-04-06 12:24:32 +0900

    Add dto to entity.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/service/util/OrderMapper.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/service/util/OrderMapper.java
@@ -2,9 +2,22 @@ package maybe_all_here.orderservice.service.util;
 
 import maybe_all_here.orderservice.domain.Orders;
 import maybe_all_here.orderservice.dto.order.OrderProvideResponse;
+import maybe_all_here.orderservice.dto.order.OrderRequest;
 import maybe_all_here.orderservice.utility.CommonUtils;
 
 public class OrderMapper {
+
+    public static Orders dtoToEntity(OrderRequest orderRequest) {
+        return Orders.builder()
+                .itemTitle(orderRequest.getItemTitle())
+                .orderQuantity(orderRequest.getOrderQuantity())
+                .totalPrice(orderRequest.getTotalPrice())
+                .discountPrice(orderRequest.getSpentMileage())
+                .email(orderRequest.getEmail())
+                .itemId(orderRequest.getItemId())
+                .orderState(orderRequest.getOrderState())
+                .build();
+    }
 
     public static OrderProvideResponse entityToProvideDto(Orders orders) {
         if (CommonUtils.isNull(orders)) {
