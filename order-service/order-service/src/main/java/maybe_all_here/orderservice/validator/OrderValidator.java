34d9807edff368ba356440da2c514b168703742f
liveforone <yc4852@gmail.com>
2023-04-07 10:51:38 +0900

    Add order cancel validator.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/validator/OrderValidator.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/validator/OrderValidator.java
@@ -1,14 +1,20 @@
 package maybe_all_here.orderservice.validator;
 
 import lombok.RequiredArgsConstructor;
+import maybe_all_here.orderservice.domain.Orders;
 import maybe_all_here.orderservice.dto.item.ItemProvideResponse;
 import maybe_all_here.orderservice.dto.mileage.MileageResponse;
 import maybe_all_here.orderservice.dto.order.OrderRequest;
+import maybe_all_here.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
 public class OrderValidator {
+
+    private final OrderRepository orderRepository;
 
     private static final int SOLD_OUT = 0;
 
@@ -22,5 +28,27 @@ public class OrderValidator {
 
     public boolean isOverMileage(OrderRequest orderRequest, MileageResponse mileage) {
         return orderRequest.getSpentMileage() > mileage.getMileagePoint();
+    }
+
+    public boolean isOverCancelLimitDate(Long orderId) {
+        Orders orders = orderRepository.findOneById(orderId);
+        LocalDate createdDate = orders.getCreatedDate();
+        int orderDate = createdDate.getDayOfYear();
+
+        int nowYear = LocalDate.now().getYear();
+        int nowDate = LocalDate.now().getDayOfYear();
+
+        int cancelLimitDate = switch (orderDate) {
+            case 359 -> LocalDate.of(nowYear, 1, 1).getDayOfYear();
+            case 360 -> LocalDate.of(nowYear, 1, 2).getDayOfYear();
+            case 361 -> LocalDate.of(nowYear, 1, 3).getDayOfYear();
+            case 362 -> LocalDate.of(nowYear, 1, 4).getDayOfYear();
+            case 363 -> LocalDate.of(nowYear, 1, 5).getDayOfYear();
+            case 364 -> LocalDate.of(nowYear, 1, 6).getDayOfYear();
+            case 365 -> LocalDate.of(nowYear, 1, 7).getDayOfYear();
+            default -> orderDate + 7;
+        };
+
+        return nowDate > cancelLimitDate;
     }
 }
