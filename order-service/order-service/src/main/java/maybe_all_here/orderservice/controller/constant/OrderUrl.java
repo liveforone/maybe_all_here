552d12d133582a4cf543b8902da5b975f821a273
liveforone <yc4852@gmail.com>
2023-04-07 10:13:30 +0900

    Add order list url.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/OrderUrl.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/OrderUrl.java
@@ -3,5 +3,6 @@ package maybe_all_here.orderservice.controller.constant;
 public class OrderUrl {
 
     public static final String ORDER_DETAIL = "/order/detail/{orderId}";
+    public static final String ORDER_LIST = "/orders";
     public static final String ORDER = "/order/{itemId}";
 }
