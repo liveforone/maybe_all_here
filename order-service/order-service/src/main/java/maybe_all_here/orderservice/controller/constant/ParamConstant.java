9689c1e78fd409bde0cd50987b0f229dd720948d
liveforone <yc4852@gmail.com>
2023-04-07 10:20:03 +0900

    Add last id and page size constant.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/ParamConstant.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/ParamConstant.java
@@ -4,4 +4,6 @@ public class ParamConstant {
 
     public static final String ORDER_ID = "orderId";
     public static final String ITEM_ID = "itemId";
+    public static final String LAST_ID = "lastId";
+    public static final String PAGE_SIZE = "pageSize";
 }
