1b8c65cfbc87b9e36b4d24cfb31bdcd703571be9
liveforone <yc4852@gmail.com>
2023-04-08 13:54:05 +0900

    Refactor create review url.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/controller/constant/ReviewUrl.java
@@ -4,7 +4,7 @@ public class ReviewUrl {
 
     public static final String REVIEWS_BELONG_TO_ITEM = "/reviews/{itemId}";
     public static final String REVIEW_DETAIL = "/review/{reviewId}";
-    public static final String CREATE_REVIEW = "/create-review/{itemId}";
+    public static final String CREATE_REVIEW = "/create-review/item/{itemId}/order/{orderId}";
     public static final String EDIT_REVIEW = "/edit-review/{reviewId}";
     public static final String DELETE_REVIEW = "/delete-review/{reviewId}";
 }
