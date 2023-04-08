2401e4fa80f7336bb5d7cc8c5af2ed9985dc048e
liveforone <yc4852@gmail.com>
2023-04-08 14:24:49 +0900

    Refactor provide url.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/OrderProvideUrl.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/constant/OrderProvideUrl.java
@@ -2,5 +2,5 @@ package maybe_all_here.orderservice.controller.constant;
 
 public class OrderProvideUrl {
 
-    public static final String ORDER_INFO = "/order-info/email/{email}/item/{itemId}";
+    public static final String ORDER_INFO = "/order-info/{orderId}";
 }
