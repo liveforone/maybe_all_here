7377c49295c4e4200bbced947a72e0afdd402e94
liveforone <yc4852@gmail.com>
2023-04-08 14:24:42 +0900

    Refactor provide controller param.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/OrderProvideController.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/controller/OrderProvideController.java
@@ -2,7 +2,7 @@ package maybe_all_here.orderservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import maybe_all_here.orderservice.controller.constant.OrderProvideUrl;
-import maybe_all_here.orderservice.controller.constant.ProvideParamConstant;
+import maybe_all_here.orderservice.controller.constant.ParamConstant;
 import maybe_all_here.orderservice.dto.order.OrderProvideResponse;
 import maybe_all_here.orderservice.service.OrderService;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +17,8 @@ public class OrderProvideController {
     private final OrderService orderService;
 
     @PostMapping(OrderProvideUrl.ORDER_INFO)
-    public ResponseEntity<?> getOrderInfo(
-            @PathVariable(ProvideParamConstant.EMAIL) String email,
-            @PathVariable(ProvideParamConstant.ITEM_ID) Long itemId
-    ) {
-        OrderProvideResponse order = orderService.getOrderByEmailAndItemId(email, itemId);
+    public ResponseEntity<?> getOrderInfo(@PathVariable(ParamConstant.ORDER_ID) Long orderId) {
+        OrderProvideResponse order = orderService.getProvideOrderById(orderId);
         return ResponseEntity.ok(order);
     }
 }
