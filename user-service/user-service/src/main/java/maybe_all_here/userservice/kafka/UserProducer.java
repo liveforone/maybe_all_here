38ca36f1fd8473af2b0f7bf6ca4dc7b9b758ea5c
liveforone <yc4852@gmail.com>
2023-04-12 22:53:29 +0900

    Add async in command method.

--- user-service/user-service/src/main/java/maybe_all_here/userservice/kafka/UserProducer.java
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/kafka/UserProducer.java
@@ -3,9 +3,11 @@ package maybe_all_here.userservice.kafka;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.userservice.async.AsyncConstant;
 import maybe_all_here.userservice.kafka.constant.KafkaLog;
 import maybe_all_here.userservice.kafka.constant.Topic;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +18,7 @@ public class UserProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
     Gson gson = new Gson();
 
+    @Async(AsyncConstant.commandAsync)
     public void createMileage(String email) {
         String jsonOrder = gson.toJson(email);
 
