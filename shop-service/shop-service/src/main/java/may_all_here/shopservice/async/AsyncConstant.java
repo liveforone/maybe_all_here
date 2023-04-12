688e5659b070d278b0e80fc075c721e9a46d49c5
liveforone <yc4852@gmail.com>
2023-04-12 23:23:21 +0900

    Add async config.

---
+++ shop-service/shop-service/src/main/java/may_all_here/shopservice/async/AsyncConstant.java
@@ -0,0 +1,8 @@
+package may_all_here.shopservice.async;
+
+public class AsyncConstant {
+    public static final String commandAsync = "commandAsync";
+    public static final int CORE_POOL_SIZE = 10;
+    public static final int MAX_POOL_SIZE = 20;
+    public static final int QUEUE_CAPACITY = 100;
+}
