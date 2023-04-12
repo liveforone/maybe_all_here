052df4e72e64eb854b7e0d45885cff42de9a404c
liveforone <yc4852@gmail.com>
2023-04-12 23:13:16 +0900

    Remove dto to entity mappper.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/util/MileageMapper.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/service/util/MileageMapper.java
@@ -5,12 +5,6 @@ import maybe_all_here.mileageservice.dto.mileage.MileageResponse;
 
 public class MileageMapper {
 
-    public static Mileage createMileage(String email) {
-        return Mileage.builder()
-                .email(email)
-                .build();
-    }
-
     public static MileageResponse entityToDto(Mileage mileage) {
         return MileageResponse.builder()
                 .email(mileage.getEmail())
