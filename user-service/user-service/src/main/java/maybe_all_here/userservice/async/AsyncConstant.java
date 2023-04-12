bd595519b988e756a4c7e5668736c31267e670f3
liveforone <yc4852@gmail.com>
2023-04-12 22:44:34 +0900

    Add async config.

---
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/async/AsyncConstant.java
@@ -0,0 +1,8 @@
+package maybe_all_here.userservice.async;
+
+public class AsyncConstant {
+    public static final String serviceAsync = "commandAsync";
+    public static final int CORE_POOL_SIZE = 10;
+    public static final int MAX_POOL_SIZE = 20;
+    public static final int QUEUE_CAPACITY = 100;
+}
