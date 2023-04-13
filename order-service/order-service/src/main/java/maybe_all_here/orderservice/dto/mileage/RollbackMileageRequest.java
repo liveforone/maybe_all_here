e5013343958253614eb84908107adacab89a4553
liveforone <yc4852@gmail.com>
2023-04-13 13:58:24 +0900

    Add rollback mileage request dto.

---
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/mileage/RollbackMileageRequest.java
@@ -0,0 +1,15 @@
+package maybe_all_here.orderservice.dto.mileage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RollbackMileageRequest {
+    private long spentMileage;
+    private String email;
+}
