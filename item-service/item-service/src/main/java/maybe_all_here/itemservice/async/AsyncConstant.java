a4bcdbc9c39386730fe275e0d099abf695b0eda1
liveforone <yc4852@gmail.com>
2023-04-13 11:03:58 +0900

    Add async config.

---
+++ item-service/item-service/src/main/java/maybe_all_here/itemservice/async/AsyncConstant.java
@@ -0,0 +1,8 @@
+package maybe_all_here.itemservice.async;
+
+public class AsyncConstant {
+    public static final String commandAsync = "commandAsync";
+    public static final int CORE_POOL_SIZE = 10;
+    public static final int MAX_POOL_SIZE = 20;
+    public static final int QUEUE_CAPACITY = 100;
+}
