4f502444b5c6406f0b6bed4458d269c3fbc63578
liveforone <yc4852@gmail.com>
2023-04-13 14:19:26 +0900

    Refactor accmulate policy test.

--- mileage-service/mileage-service/src/test/java/maybe_all_here/mileageservice/kafka/util/AccumulatePolicyTest.java
+++ mileage-service/mileage-service/src/test/java/maybe_all_here/mileageservice/kafka/util/AccumulatePolicyTest.java
@@ -1,12 +1,8 @@
 package maybe_all_here.mileageservice.kafka.util;
 
-import maybe_all_here.mileageservice.dto.updateMileage.AccumulateRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AccumulatePolicyTest {
@@ -15,11 +11,9 @@ class AccumulatePolicyTest {
     void calculateAccumulate() {
         //given
         long orderPrice = 43000;
-        AccumulateRequest request = new AccumulateRequest();
-        request.setOrderPrice(orderPrice);
 
         //when
-        long calculatedMileage = AccumulatePolicy.calculateAccumulate(request);
+        long calculatedMileage = AccumulatePolicy.calculateAccumulate(orderPrice);
 
         //then
         long accumulate = 430;
