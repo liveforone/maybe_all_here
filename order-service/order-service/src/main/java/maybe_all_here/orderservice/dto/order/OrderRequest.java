5d94f467fdf0f950913e54aa3b2ca791deec58f4
liveforone <yc4852@gmail.com>
2023-04-13 13:13:58 +0900

    Refactor dto style for ddd.

--- order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderRequest.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/dto/order/OrderRequest.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import maybe_all_here.orderservice.domain.OrderState;
 
 @Data
 @Builder
@@ -15,21 +14,11 @@ import maybe_all_here.orderservice.domain.OrderState;
 @AllArgsConstructor
 public class OrderRequest {
 
-    private Long id;
-    private String itemTitle;
-
     @Positive(message = "주문 수량은 반드시 0 이상이여야 합니다.")
     @NotNull(message = "주문하려는 수량을 반드시 기재해주세요.")
     private long orderQuantity;
 
-    private long totalPrice;
-    private long discountedPrice;
-
     @PositiveOrZero(message = "사용하려는 마일리지는 양수로 작성해주세요.")
     @NotNull(message = "사용하려는 마일리지는 반드시 기재해주세요.")
     private long spentMileage;
-
-    private String email;
-    private Long itemId;
-    private OrderState orderState;
 }
