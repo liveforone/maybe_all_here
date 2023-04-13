fde942a23fa8e069b1d9b940aa0e6137526a3bb4
liveforone <yc4852@gmail.com>
2023-04-13 13:58:36 +0900

    Add rollback mileage kafka topic.

--- mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/kafka/constant/Topic.java
+++ mileage-service/mileage-service/src/main/java/maybe_all_here/mileageservice/kafka/constant/Topic.java
@@ -5,4 +5,5 @@ public class Topic {
     public static final String CREATE_MILEAGE = "create-mileage";
     public static final String INCREASE_MILEAGE = "increase-mileage";
     public static final String DECREASE_MILEAGE = "decrease-mileage";
+    public static final String ROLLBACK_MILEAGE = "rollback-mileage";
 }
--- order-service/order-service/src/main/java/maybe_all_here/orderservice/kafka/constant/Topic.java
+++ order-service/order-service/src/main/java/maybe_all_here/orderservice/kafka/constant/Topic.java
@@ -1,10 +1,10 @@
 package maybe_all_here.orderservice.kafka.constant;
 
 public class Topic {
-
-    public static final String ROLLBACK_REMAINING = "rollback-remaining";
     public static final String DECREASE_REMAINING = "decrease-remaining";
     public static final String INCREASE_MILEAGE = "increase-mileage";
     public static final String DECREASE_MILEAGE = "decrease-mileage";
+    public static final String ROLLBACK_REMAINING = "rollback-remaining";
+    public static final String ROLLBACK_MILEAGE = "rollback-mileage";
     public static final String REMOVE_REVIEW_BELONG_ORDER = "remove-review-belong-order";
 }
