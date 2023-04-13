443f584997777c7efb4465cafc07bdb9b9df12bb
liveforone <yc4852@gmail.com>
2023-04-13 19:25:10 +0900

    Add async in consumer method.

--- review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewConsumer.java
+++ review-service/review-service/src/main/java/maybe_all_here/reviewservice/kafka/ReviewConsumer.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maybe_all_here.reviewservice.async.AsyncConstant;
 import maybe_all_here.reviewservice.kafka.constant.KafkaLog;
 import maybe_all_here.reviewservice.kafka.constant.Topic;
 import maybe_all_here.reviewservice.repository.ReviewRepository;
 import maybe_all_here.reviewservice.utility.CommonUtils;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,7 @@ public class ReviewConsumer {
     ObjectMapper objectMapper = new ObjectMapper();
 
     @KafkaListener(topics = Topic.REMOVE_REVIEW_BELONG_ORDER)
+    @Async(AsyncConstant.commandAsync)
     @Transactional
     public void removeReviewBelongOrder(String kafkaMessage) throws JsonProcessingException {
         log.info(KafkaLog.KAFKA_RECEIVE_LOG.getValue() + kafkaMessage);
@@ -36,6 +39,7 @@ public class ReviewConsumer {
     }
 
     @KafkaListener(topics = Topic.REMOVE_REVIEW_BELONG_ITEM)
+    @Async(AsyncConstant.commandAsync)
     @Transactional
     public void removeReviewBelongItem(String kafkaMessage) throws JsonProcessingException {
         log.info(KafkaLog.KAFKA_RECEIVE_LOG.getValue() + kafkaMessage);
