5dcfea63660d9098dfa0cb4d0b484cc41948cb95
liveforone <yc4852@gmail.com>
2023-05-09 14:59:45 +0900

    Refactor email change to username;

--- user-service/user-service/src/main/java/maybe_all_here/userservice/service/CustomUserDetailService.java
+++ user-service/user-service/src/main/java/maybe_all_here/userservice/service/CustomUserDetailService.java
@@ -17,29 +17,29 @@ public class CustomUserDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return createUserDetails(memberRepository.findByEmail(email));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return createUserDetails(memberRepository.findByEmail(username));
     }
 
     private UserDetails createUserDetails(Member member) {
         if (member.getAuth() == Role.ADMIN) {
             String ADMIN_ROLE = "ADMIN";
             return User.builder()
-                    .username(member.getEmail())
+                    .username(member.getUsername())
                     .password(member.getPassword())
                     .roles(ADMIN_ROLE)
                     .build();
         } else if(member.getAuth() == Role.SELLER) {
             String SELLER_ROLE = "SELLER";
             return User.builder()
-                    .username(member.getEmail())
+                    .username(member.getUsername())
                     .password(member.getPassword())
                     .roles(SELLER_ROLE)
                     .build();
         } else {
             String MEMBER_ROLE = "MEMBER";
             return User.builder()
-                    .username(member.getEmail())
+                    .username(member.getUsername())
                     .password(member.getPassword())
                     .roles(MEMBER_ROLE)
                     .build();
